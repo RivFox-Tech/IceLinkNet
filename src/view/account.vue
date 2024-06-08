389948370d65d76b6ec198cf6bed3fb8e1503634
farish-awa <heliuliua@outlook.com>
2024-06-08 21:45:19 +0800

    修正

--- src/view/account.vue
+++ src/view/account.vue
@@ -4,9 +4,9 @@
       <n-space>
         <n-avatar round :size="64" src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg" />
         <div>
-          <span><b>Farish</b></span> <br>
-          <span>heliuliua@outlook.com</span> <br>
-          <span>云隧道 0/ 20</span>
+          <span><b>xxx</b></span> <br>
+          <span>xxx@xxx.com</span> <br>
+          <span>云隧道 0/20</span>
         </div>
       </n-space>
     </n-card>
